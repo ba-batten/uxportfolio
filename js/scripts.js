0afd4f1e8d0bf9ca5a82a107a70e088856a5c83b
Brandon Batten <brandon@MacBook-Pro.local>
2020-06-29 14:56:55 -0400

    Initial project commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Global variables
+let url = window.location.href.toString();
+
+// Function definitions
+let showNavLocation = function (url) {
+
+  if (url.includes('index')) {
+    document.getElementById('home').classList.toggle('nav-locator');
+  }
+
+  else if (url.includes('projects')) {
+    document.getElementById('projects').classList.toggle('nav-locator');
+  }
+
+  else if (url.includes('about')) {
+    document.getElementById('about').classList.toggle('nav-locator');
+  }
+}
+
+let placeFooter = function(url) {
+  if (url.includes('index')) {
+    document.getElementById('footer').classList.toggle('short-page-footer');
+  }
+}
+
+
+
+// Indicate location by changing color of 1st level nav link
+showNavLocation(url);
+
+// Position footer on short pages
+placeFooter(url);
